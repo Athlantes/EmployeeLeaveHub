63757459d574e1361bbc2f9182b89f9a1690619e
Athlantes <caprarugabriel@gmail.com>
2026-07-21 11:33:12 +0300

    Employee Leave Hub

---
+++ leavehub/src/main/java/org/athisworkshop/leavehub/LeavehubApplication.java
@@ -0,0 +1,13 @@
+package org.athisworkshop.leavehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeavehubApplication.class, args);
+    }
+
+}
---
+++ leavehub/src/test/java/org/athisworkshop/leavehub/LeavehubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.athisworkshop.leavehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeavehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
