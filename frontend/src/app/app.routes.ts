01d4eb5cb9b479e2ff017281be62164822f614f6
Athlantes <caprarugabriel@gmail.com>
2026-07-28 15:48:36 +0300

    Fixat legatura Login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Login } from './login/login';
+import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard'; 
 
 export const routes: Routes = [
